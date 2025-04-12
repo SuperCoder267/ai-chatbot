8446f7029345b7e9c46beed15398d92fe4c248a6
SuperCoder267 <ahmadfarooq710@gmail.com>
2025-04-12 16:42:58 -0400

    Create domElements.js

---
+++ scripts/domElements.js
@@ -0,0 +1,3 @@
+export const userInput = document.getElementById('userInput');
+export const chatbox = document.getElementById('chatbox');
+export const clearChatBtn = document.getElementById('clearChat');
